fb7bfcedfb8be87e734efd3d8845aa6c1c309992
Chris Rodgers <christoph.rodgers@gmail.com>
2024-12-05 19:00:06 +1300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -95,7 +95,8 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "https://innoccull.github.io/",
+  base: "/crodgers",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
